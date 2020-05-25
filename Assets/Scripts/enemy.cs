7d7fc58497a9f718057083f8444ad8820a632d1f
rogercs99 <rogercampos18@gmail.com>
2020-05-25 19:01:41 +0200

    Roger s2

---
+++ Assets/Scripts/enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+
+	public Vector3 movementSpeed; //1
+	public Space space; //2
+	private enemy_spawner enemyspawner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetSpawner(enemy_spawner spawner)
+	{
+	    enemyspawner = spawner;
+	}
+
+}
---
+++ Assets/Scripts/enemy_spawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_spawner : MonoBehaviour
+{
+    public bool canSpawn = true; // 1
+	public GameObject enemyPrefab; // 2
+	public List<Transform> enemySpawnPositions = new List<Transform>(); // 3
+	public float timeBetweenSpawns; // 4
+	private List<GameObject> enemyList = new List<GameObject>(); // 5
+
+
+	void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+   private void Spawnenemy()
+	{
+	    Vector3 randomPosition = enemySpawnPositions[Random.Range(0, enemySpawnPositions.Count)].position; // 1
+	    GameObject enemy = Instantiate(enemyPrefab, randomPosition, enemyPrefab.transform.rotation); // 2
+	    enemyList.Add(enemy); // 3
+	    enemy.GetComponent<enemy>().SetSpawner(this); // 4
+	}
+
+
+	private IEnumerator SpawnRoutine() // 1
+	{
+	    while (canSpawn) // 2
+	    {
+	        Spawnenemy(); // 3
+	        yield return new WaitForSeconds(timeBetweenSpawns); // 4
+	    }
+	}
+
+	public void RemoveenemyFromList(GameObject enemy)
+	{
+	    enemyList.Remove(enemy);
+	}
+
+}
+
