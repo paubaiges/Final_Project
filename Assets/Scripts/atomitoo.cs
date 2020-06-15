0b9752bfb731ff9a79805e9a407aba8458cfcd78
rogercs99 <rogercampos18@gmail.com>
2020-06-15 19:14:29 +0200

    Roger

---
+++ Assets/Scripts/atomitoo.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class atomitoo : MonoBehaviour
+{
+
+	public GameObject yo;
+    private Collider myCollider;
+    private Rigidbody myRigidbody;
+    public int contador;
+
+    void Start()
+    {
+        myCollider = GetComponent<Collider>();
+        myRigidbody = GetComponent<Rigidbody>();
+        contador = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	transform.Rotate(0, 0 , 0);
+
+        if(contador == 1){
+            yo.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("palo"))
+        {
+  			contador = contador +1;
+        }
+
+    }
+
+
+
+}
---
+++ Assets/Scripts/plane.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plane : MonoBehaviour
+{
+    public GameObject palo;
+    public int contador;
+    private Collider myCollider;
+    private Rigidbody myRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCollider = GetComponent<Collider>();
+        myRigidbody = GetComponent<Rigidbody>();
+        contador = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(contador == 2){
+        	palo.SetActive(true);
+        }
+
+        if(contador == 4){
+        	palo.active = false;
+        	contador = 0;
+        }
+    }
+
+    
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Espada"))
+        {
+  			contador = contador + 1;
+        }
+
+    }
+    
+}
